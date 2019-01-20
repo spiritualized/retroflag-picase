76cf3163efc79bdce83646337128983f76aa10fb
m4rcSA <43429867+m4rcSA@users.noreply.github.com>
2019-01-20 06:51:44 +0100

    added the missing script variable
    now deletes the old ones, and downloads the scripts

--- libreelec_install.sh
+++ libreelec_install.sh
@@ -3,17 +3,21 @@ cd /storage/
 mkdir scripts
 cd /storage/scripts
 
+script=retroflag-picase-libreelec-master/SafeShutdown.py
+
 if [ -e $script ];
 	then
 		echo "Script SafeShutdown.py already exists. Reinstalling..."
-		rm -R retroflag-picase-libreelec
-	else
-		wget "https://github.com/spiritualized/retroflag-picase-libreelec/archive/master.zip"
-		unzip master.zip
-		cd retroflag-picase-libreelec-master
-		chmod +x libreelec-shutdown.sh
-		chmod +x libreelec-restart.sh
+		rm master.zip
+		rm -R retroflag-picase-libreelec-master
 fi
+
+wget "https://github.com/spiritualized/retroflag-picase-libreelec/archive/master.zip"
+unzip master.zip
+cd retroflag-picase-libreelec-master
+chmod +x libreelec-shutdown.sh
+chmod +x libreelec-restart.sh
+
 #-----------------------------------------------------------
 
 #Step 2) Enable Python script to run on start up------------
